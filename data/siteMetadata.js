bdd3e87b5463575dc1be49ce25c9c544601006f5
Michael Shi <mfshi03@gmail.com>
2022-09-25 01:59:39 -0400

    changed metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,21 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'mfshi03 Blog',
+  author: 'Michael Shi',
+  headerTitle: 'mfshi03 Blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteLogo: '/static/images/godgate.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'mfshi03@gmail.com',
+  github: 'https://github.com/mfshi03',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/michael-shi-33304020b/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
