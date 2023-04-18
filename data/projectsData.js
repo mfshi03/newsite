5ed7b69060167304e3bab995efd0952854528329
Michael Shi <mfshi03@gmail.com>
2023-04-18 13:22:08 -0400

    adding my projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,6 +5,18 @@ const projectsData = [
     imgSrc: '/static/images/Cluster-sharding.png',
     href: 'https://github.com/mfshi03/Akka-Cluster-Sharding',
   },
+  {
+    title: 'LinkBunny',
+    description: `Trying to improve search on google using a GPT-based chrome extension that scrapes content from websites`,
+    imgSrc: '/static/images/linkbunny.png',
+    href: 'https://chrome.google.com/webstore/detail/linkbunny/khdlilmahhmmcginjoligfnknggoemph/related?hl=en&authuser=0',
+  },
+  {
+    title: 'VioletIQ',
+    description: `Simple project to improve studying information on websites using GPTIndex and Streamlit`,
+    imgSrc: '/static/images/violetiq.png',
+    href: 'https://devpost.com/software/idk-51zxlk',
+  },
 ]
 
 export default projectsData
