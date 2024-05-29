aad7c7df3cd432f49bd8e03c2889767d0ee65b4a
Michael Shi <mfshi03@gmail.com>
2024-05-29 17:17:27 -0400

    new logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,13 +1,17 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import LogoBlack from '@/data/resized_black_piece.svg'
+import LogoWhite from '@/data/resized_white_piece.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import { useTheme } from 'next-themes'
 
 const LayoutWrapper = ({ children }) => {
+  const { theme, resolvedTheme } = useTheme()
+  const currentTheme = theme || resolvedTheme
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
@@ -16,7 +20,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  {currentTheme === 'dark' ? <LogoWhite /> : <LogoBlack />}
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
