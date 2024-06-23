ab3ceace3bbb290d02cbd9628268f94d94e178a1
Michael Shi <mfshi03@gmail.com>
2024-06-23 19:26:19 -0400

    default prettier settings

--- prettier.config.js
+++ prettier.config.js
@@ -6,17 +6,4 @@ module.exports = {
   useTabs: false,
   trailingComma: 'es5',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
-  proseWrap: 'preserve',
-  htmlWhitespaceSensitivity: 'strict',
-  endOfLine: 'lf',
-  overrides: [
-    {
-      files: '*.mdx',
-      options: {
-        parser: 'mdx',
-        proseWrap: 'preserve',
-      },
-    },
-  ],
 }
